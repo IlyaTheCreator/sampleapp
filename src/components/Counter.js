5af5a90cbb9e985f737ed4c34b162f7df2471020
IlyaTheCreator <respondatanytime@gmail.com>
2021-04-02 09:44:42 +0300

    asdfa

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,12 @@
-// import { useState } from "react"
-import { useDispatch, useSelector} from "react-redux"
+import { useState } from "react"
 import "./Counter.css"
 
 const Counter = () => {
-    // const [counter, setCounter] = useState(0)
-    const counter = useSelector(state => state.counter)
-    const dispatch = useDispatch()
-
+    const [counter, setCounter] = useState(0)
     return (
         <div className="Counter">
             <p>{counter}</p>
-            {/* <button onClick={() => setCounter(counter + 1)}>click</button> */}
-            <button onClick={() => dispatch({type: "INCREMENT", payload: {}})}>click</button>
+            <button onClick={() => setCounter(counter + 1)}>click</button>
         </div>
     )
 }
